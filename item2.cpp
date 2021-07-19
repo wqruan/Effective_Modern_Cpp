c25c3bd7a720212afcdb210144e2dc1336bbeb53
wqruan <wenqqiangruan@gmail.com>
2021-07-19 16:54:11 +0800

    read item2

---
+++ item2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template<typename T>
+void f(T param) {
+    printf("%d\n", param);
+}
+
+int main() {
+    auto x = 27;
+    auto cx(27);
+    auto x3 = { 27, 28, 29 };
+    f(x);
+    f(cx);
+    f(x3);
+}
