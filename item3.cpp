c54281decfa9f4ef7cbf33ee402ff65ebdcac04f
阮文强 <wenqqiangruan@gmail.com>
2021-07-20 22:58:28 +0800

    first use clang

--- item3.cpp
+++ item3.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
 template<typename Container, typename Index>
-auto authAndAccess(Container& c, Index i) {
+auto authAndAccess(Container& c, Index i) ->decltype(c[i]) {
     return c[i];
 }
+
 
 int main() {
     int c[3] = {1,2,3};
     authAndAccess(c, 1);
-}
+} 
