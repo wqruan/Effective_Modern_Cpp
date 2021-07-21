49b0b6ffd2dfd5d0fb6e65109786485f2c91b1c1
wqruan <wenqqiangruan@gmail.com>
2021-07-21 19:52:40 +0800

    finish item4

---
+++ item4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+#include <boost/type_index.hpp>
+template<typename T>
+void f(const T& param)
+{
+ using std::cout;
+ using boost::typeindex::type_id_with_cvr;
+ // show T
+ cout << "T = "
+ << type_id_with_cvr<T>().pretty_name()
+ << '\n';
+ // show param's type
+ cout << "param = "
+ << type_id_with_cvr<decltype(param)>().pretty_name()
+ << '\n';
+}
+
+int main() {
+    const int theAnswer = 42;
+    auto x = "aas";
+    auto y = &theAnswer;
+    std::cout << typeid(x).name() << std::endl;
+    std::cout << typeid(y).name() << std::endl;    
+    f(y);
+}
