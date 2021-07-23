17d012d56e49686aa978726ff3a3f2925e0420ed
阮文强 <wenqqiangruan@gmail.com>
2021-07-23 23:19:23 +0800

    finish item5

--- item5.cpp
+++ item5.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-
-
+#include <map>
+#include <string>
 int main() {
-    auto x2;
-    x2= 1;
+    auto x2= 1;
+    std::map<std::string, int> store;
+    store["wqruan"] = 0;
+    for (auto p:store) {
+        std::cout << p.first  << std::endl;
+    }
 }
